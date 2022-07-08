1880260aae9f593c844ecca93ad17cfb49124dbe
Easy Z <easyz@bitnp.net>
2022-07-08 11:35:32 +0800

    init poller

---
+++ src/Poller/Poller.h
@@ -0,0 +1,14 @@
+#ifndef __SRC_POLLER_POLLER_H_
+#define __SRC_POLLER_POLLER_H_
+
+class Poller {
+private:
+    static const int MAXFDS = 65535;
+    int _pollerFD;
+
+public:
+    
+
+};
+
+#endif /* __SRC_POLLER_POLLER_H_ */
